f551769fc803736f40af85a569b2482f96a428ad
Saikrishna1504 <saikrishna26918@gmail.com>
2026-07-26 06:35:00 +0000

    [ax_sandbox] Implement "Hide from Launcher" option
    Change-Id: I12e7051e07eb7aab6ef0d963dd2e0149a0f5c032
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -20021,6 +20021,12 @@ public class ActivityManagerService extends IActivityManager.Stub
     }
 
     @Override
+    public boolean isSandboxPackageHiddenFromLauncher(String packageName) {
+        final IAxSandboxService service = getAxSandboxService();
+        return service != null && service.isPackageHiddenFromLauncher(packageName);
+    }
+
+    @Override
     public void addSandboxLockedApp(String packageName) {
         final IAxSandboxService service = getAxSandboxService();
         if (service != null) {
@@ -20045,6 +20051,14 @@ public class ActivityManagerService extends IActivityManager.Stub
     }
 
     @Override
+    public void setSandboxPackageHiddenFromLauncher(String packageName, boolean hidden) {
+        final IAxSandboxService service = getAxSandboxService();
+        if (service != null) {
+            service.setPackageHiddenFromLauncher(packageName, hidden);
+        }
+    }
+
+    @Override
     public List<String> getSandboxLockedPackages() {
         final IAxSandboxService service = getAxSandboxService();
         return service != null ? service.getLockedPackages() : Collections.emptyList();
@@ -20054,6 +20068,12 @@ public class ActivityManagerService extends IActivityManager.Stub
     public List<String> getSandboxHiddenPackages() {
         final IAxSandboxService service = getAxSandboxService();
         return service != null ? service.getHiddenPackages() : Collections.emptyList();
+    }
+
+    @Override
+    public List<String> getSandboxHiddenFromLauncherPackages() {
+        final IAxSandboxService service = getAxSandboxService();
+        return service != null ? service.getHiddenFromLauncherPackages() : Collections.emptyList();
     }
 
     @Override
